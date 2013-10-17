01ee00a4dca3bae16a2313af91c983254e485a25
petealbertson <palbertson@Petes-MacBook-Air.local>
2013-10-17 18:31:53 -0500

    Initial commit for pig_latin

--- pig_latin.rb
+++ pig_latin.rb
@@ -27,8 +27,41 @@
 
 # Here are some sentences to get you started:
 
-# address = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
+address = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
 # ruby = "Ruby gives us lots of data containers to choose from"
 
 # oodgay ucklay!
+vowels = ["a", "e", "i", "o", "u"]
+
+addy_array = address.split
+
+addy_array.each do |word|
+  if word.start_with?("a", "e", "i", "o", "u")
+    print "#{word}ay "
+  else
+    new_word = word.split(//, 2)
+    print new_word[1] + new_word[0] + "ay "
+  end
+end
+
+print "#{word} "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
